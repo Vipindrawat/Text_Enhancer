c36db32c8bd082cb5fb59443b517f9f294e901d3
Vipindrawat <vipinrawat7500@gmail.com>
2026-01-17 14:59:34 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Text_Enhancer/",
   plugins: [react()],
   server: {
     port: 4000
